0ff9a205d5b73753cc259841b3997427bba1471f
murtaza <murtazamalik1319@gmail.com>
2023-01-19 23:32:34 +0530

    Refactor constructor

---
+++ MoodAnalyser.java
@@ -0,0 +1,19 @@
+package Constructor.tc1;
+
+public class MoodAnalyser {
+    public static void main(String[] args) {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String happyMood = moodAnalyser.analyseMood("I am in Happy Mood");
+        System.out.println("MY Mood is : " + happyMood);
+        String sadMood = moodAnalyser.analyseMood("I am in Sad Mood");
+        System.out.println("My Mood is : " + sadMood);
+    }
+
+    public String analyseMood(String message) {
+        if (message.toLowerCase().contains("sad"))
+            return "SAD";
+        else if (message.toLowerCase().contains("happy"))
+            return "HAPPY";
+        return message;
+    }
+}
